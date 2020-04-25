4500e3076189c08508b67fa73d5539b163fef27f
Andy Sanchez <andysanchez726@gmail.com>
2020-04-24 20:56:05 -0500

    added 5 day forcast, need to make function to remove any previous forecast

--- assets/js/onecall.js
+++ assets/js/onecall.js
@@ -7,11 +7,33 @@ var body = document.querySelector("body")
 var testWeatherEl = document.querySelector("#test-weather")
 var cityForm = document.querySelector("#city-form")
 var cityInput = document.querySelector("#city-input")
+// Current weather Elements
+var cityNameDateEl = document.querySelector("#name-date")
+var cityTempEl = document.querySelector("#current-temp")
+var cityHumidityEl = document.querySelector("#current-humidity")
+var cityWindEl = document.querySelector("#current-wind")
+var cityUVEl = document.querySelector("#current-uv")
+var cityIconEl = document.querySelector("#current-icon")
+
+// forecast elements
+var forecastContainerEl = document.querySelector("#forecast-container")
+// var forecastDateEl = document.querySelector("#forecast-date")
+// var forecastTempEl = document.querySelector("#forecast-temp")
+// var forecastHumidityEl = document.querySelector("#forecast-humidity")
+// var forecastIconEl = document.querySelector("#forecast-icon")
+
+// function to get avverage of min and max temperature for forecast
+var averageTemp = function (average) {
+     var sum = average.reduce( (a,b) => a + b)/average.length
+     return sum.toFixed([0])
+}
 
 var formSubmitHandler = function (event) {
     event.preventDefault();
     var city = cityInput.value
-    currentConditions(city)
+    forecastContainerEl.remove()
+    currentConditions(city);
+    dailyForecast(city);
 }
 
 var currentConditions = function (cityEntered) {
@@ -21,30 +43,77 @@ var currentConditions = function (cityEntered) {
         return response.json()
         .then((data) => {
 
-        var testItemsContainer = document.createElement("ul")
-        var testItems = document.createElement("li")
         var cityName = data.name
         var cityLon = data.coord.lon
         var cityLat = data.coord.lat
-        console.log(cityName)
         // fetch city uv index using cityLon and cityLat
         fetch("https://api.openweathermap.org/data/2.5/onecall?lat=" + cityLat + "&lon=" + cityLon + "&units=imperial&appid=7c1bb6d4995897e4344ff7f98ec3a222")
             .then((locationResponse) => {
                 return locationResponse.json()
                 .then((locationData) => {
-                    var cityTemp = locationData.current.temp
-                    console.log(cityTemp)
+                    // current day
+                    var cityDate = moment.unix(locationData.current.dt).format("MM/DD/YYYY hh mm ss")
+                    console.log(cityDate)
+                    cityNameDateEl.textContent = cityName + " " + cityDate
+                    cityTempEl.textContent = "Temperature: " + locationData.current.temp + "°F"
+                    cityHumidityEl.textContent = "Humidity: " + locationData.current.humidity + "%"
+                    cityWindEl.textContent = "Wind Speed: " + locationData.current.wind_speed + " MPH"
+                    cityUVEl.textContent = "UV Index: " + locationData.current.uvi
+                    cityIconEl.setAttribute("src", "http://openweathermap.org/img/wn/" + locationData.current.weather[0].icon + "@2x.png")
+
                 })
             })
-
-        var iconImage = document.createElement("img")
-        // iconImage.setAttribute("src", )
-        testItems.appendChild(iconImage)
-        testItemsContainer.appendChild(testItems)
-        testWeatherEl.appendChild(testItemsContainer)
+        // var iconImage = document.createElement("img")
         })
     });
 
 };
+
+var dailyForecast = function (cityEntered) {
+    fetch("http://api.openweathermap.org/data/2.5/weather?q=" + cityEntered + "&units=imperial&appid=7c1bb6d4995897e4344ff7f98ec3a222")
+    .then((response) => {
+        return response.json()
+        .then((data) => {
+
+        var cityName = data.name
+        var cityLon = data.coord.lon
+        var cityLat = data.coord.lat
+        // fetch city uv index using cityLon and cityLat
+        fetch("https://api.openweathermap.org/data/2.5/onecall?lat=" + "30.27" + "&lon=" + "-97.74" + "&units=imperial&appid=7c1bb6d4995897e4344ff7f98ec3a222")
+            .then((locationResponse) => {
+                return locationResponse.json()
+                .then((locationData) => {
+                    var forecast = locationData.daily;
+                    for (var i = 1; i < 6; i++) {
+
+                        var day = document.createElement("div")
+                        day.setAttribute("id", "day" + i)
+                        var forecastDateEl = document.createElement("h4");
+                        forecastDateEl.textContent = moment.unix(forecast[i].dt).format("MM/DD/YYYY hh mm ss")
+                        var forecastTempEl = document.createElement("p")
+                        forecastTempEl.textContent = "Temperature: " + averageTemp([forecast[i].temp.min, forecast[i].temp.max]) + "°F"
+                        var forecastHumidityEl = document.createElement("p")
+                        forecastHumidityEl.textContent = "Humidity: " + forecast[i].humidity + "%"
+                        var forecastIconEl = document.createElement("img")
+                        forecastIconEl.setAttribute("src", "http://openweathermap.org/img/wn/" + forecast[i].weather[0].icon + "@2x.png")
+
+                        // append elements to div
+                        day.appendChild(forecastDateEl)
+                        day.appendChild(forecastTempEl)
+                        day.appendChild(forecastHumidityEl)
+                        day.appendChild(forecastIconEl)
+                        forecastContainerEl.appendChild(day)
+                    }
+                })
+            })
+        var iconImage = document.createElement("img")
+        // iconImage.setAttribute("src", )
+        })
+    });
+
+}
+
+
+
 
 cityForm.addEventListener("submit", formSubmitHandler)
